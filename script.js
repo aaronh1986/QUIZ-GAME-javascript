542b8ec58d7a058e1ecad2115a01de69854940e6
aaronh1986 <aaronharbourne@gmail.com>
2021-08-11 19:29:08 +0000

    14:00

---
+++ script.js
@@ -0,0 +1,15 @@
+const startButton = document.getElementById('start-btn');
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Don\'t worry, be HAPPY')
+    startButton.classList.add('hide');
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
